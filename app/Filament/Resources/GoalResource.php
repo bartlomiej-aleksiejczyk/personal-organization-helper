561ef3315c8814b91a8fe1c6f79fd3d90459a926
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-06-16 19:30:57 +0200

    Initial commit

---
+++ app/Filament/Resources/GoalResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GoalResource\Pages;
+use App\Filament\Resources\GoalResource\RelationManagers;
+use App\Models\Goal;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GoalResource extends Resource
+{
+    protected static ?string $model = Goal::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('project_id')
+                    ->relationship('project', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('project.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGoals::route('/'),
+            'create' => Pages\CreateGoal::route('/create'),
+            'edit' => Pages\EditGoal::route('/{record}/edit'),
+        ];
+    }
+}
